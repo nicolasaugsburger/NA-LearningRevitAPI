7689d4364f783d8ca1419ba882ff6b8f742dfc23
Nicolas Augsburger <nicolasaugsburger@hotmail.com>
2025-08-03 19:03:08 +0200

    WIP Create Views app

--- LearningRevitAPI.tab/LearningRevitAPI.panel/Create Views.pushbutton/script.py
+++ LearningRevitAPI.tab/LearningRevitAPI.panel/Create Views.pushbutton/script.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__title__ = "Nico Augsburger Template"
+__title__ = "Create Views"
 __doc__ = """Version = 1.0
 Date    = 03.08.2025
 _____________________________________________________________________
@@ -45,5 +45,36 @@ active_view  = doc.ActiveView
 active_level = active_view.GenLevel           
 
 
-view_types = FilteredElementCollecto(doc).OfClass(ViewFamilyType).ToElements()
-print(view_types)
+view_types = FilteredElementCollector(doc).OfClass(ViewFamilyType).ToElements()
+view_type_plans = [vt for vt in view_types if vt.ViewFamily == ViewFamily.FloorPlan]
+floor_plan_type = view_type_plans[0]
+
+view_names = ["PLANTA GENERAL - DISTRIBUCION",
+              "PLANTA GENERAL - DISTRIBUCION COTAS",
+              "PLANTA GENERAL - PINTURAS",
+              "PLANTA GENERAL - MOBILIARIOS",
+              "PLANTA GENERAL - PAVIMENTOS Y ZOCALOS",
+              "PLANTA GENERAL - TERMINACIONES",
+              "PLANTA GENERAL - ILUMINACION",
+              "PLANTA GENERAL - CARPINTERIAS Y MESADAS",
+              "PLANTA GENERAL - EQUIPAMIENTO Y VEGETACION",
+              "PLANTA GENERAL - CIELORRASOS Y ACUSTICOS",
+              "PLANTA GENERAL - COORDINACION INGENERIA I",
+              "PLANTA GENERAL - COORDINACION INGENIERIA II",
+              "PLANTA GENERAL - COORDINACION INGENIERIA III",
+              "PLANTA GENERAL - INDICACION I",
+              "PLANTA GENERAL - INDICACION II",
+              "PLANTA GENERAL - INDICACION III",
+              "PLANTA GENERAL - INDICACION IV",
+              "PLANTA GENERAL - INDICACION V",
+              "PLANTA GENERAL - INDICACION VI",
+              ]
+
+with Transaction(doc,'Create Floor Plan') as t:
+    t.Start()
+
+    for name in view_names:
+        view = ViewPlan.Create(doc, floor_plan_type.Id, active_level.Id)
+        view.Name = name
+
+    t.Commit()
